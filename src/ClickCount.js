af5e0fd730e43fa2783632d7edc3a513b9244fb5
Salam <markeitng1972@yahoo.com>
2026-04-18 14:27:50 +0300

    using useEffect with the counter

--- src/ClickCount.js
+++ src/ClickCount.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ClickCount = () => {
   const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    if (counter === 5) {
+      alert("لقد نقرتني 5 مرات");
+      setCounter(0);
+    }
+  }, [counter]);
   return (
     <div>
       <p>لقد نقرتني {counter} مرات</p>
