b8f42ce0332c5ed96e48c9dd0a23ea1d42f9ef8b
Salam <markeitng1972@yahoo.com>
2026-04-18 13:51:39 +0300

    Reset the project to start hooks lessons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import "./App.css";
-// import Title from "./Title";
-// import Text from "./Text";
-// import {useState} from "react";
+
 
 function App() {
-  // const handleButtonClick = () => {
-  //   console.log("اهلا بالعالم");
-  // };
-  // const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
-      {/* <Title title="اكاديمية حسوب" />
-      <Text>
-        <h3>
-          تهدف موسوعة حسوب إلى توفير محتوى تعليمي متنوع وعالي الجودة لجميع
-          المستخدمين.
-        </h3>
-        <button className="btnClick" onClick={handleButtonClick}>انقرني</button>
-      </Text>
-      <h1>{count}</h1>
-      <button className="btnClick" onClick={() => setCount(count + 1)}>زيادة</button>
-      <button className="btnClick" onClick={() => setCount(count - 1)}>نقصان</button> */}
+      
     </div>
   );
 }
